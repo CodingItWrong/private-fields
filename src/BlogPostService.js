3f6e5d3d4d308491baccf683fb50c50710806b76
Josh Justice <me@codingitwrong.com>
2018-12-03 17:22:15 -0600

    Add postComment method

--- src/BlogPostService.js
+++ src/BlogPostService.js
@@ -1,4 +1,4 @@
-import { get } from './api';
+import { get, post } from './api';
 
 export function getSummaries() {
   return get('blogPosts')
@@ -20,5 +20,43 @@ class BlogPost {
       .then(response => {
         this.body = response.body;
       });
+  }
+
+  postComment(commentData) {
+    const url = `blogPosts/${this.#id}/comments`;
+
+    const requestPayload = {
+      data: {
+        type: 'comment',
+        attributes: commentData,
+      },
+    };
+
+    return post(url, requestPayload)
+      .then(response => {
+        const { id } = response.data;
+        return new Comment({ id, blogPost: this, ...commentData });
+      })
+      .catch(error => {
+        switch (error.status) {
+        case 500:
+          throw new Error(
+            `A server error occurred when trying to comment on "${this.title}"`,
+          );
+        case 403:
+          throw new Error(
+            `Sorry, you do not have access to comment on "${this.title}".`,
+          );
+        }
+      });
+  }
+}
+
+class Comment {
+  #id;
+
+  constructor({ id, ...attributes }) {
+    this.#id = id;
+    Object.assign(this, attributes);
   }
 }
